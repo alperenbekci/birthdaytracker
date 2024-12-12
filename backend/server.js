8f1071e8de7a31954dda8678da1989cc856aaa2e
alperenbekci <alperenbekci@hotmail.com>
2024-12-13 00:49:42 +0300

    chore: remove debugging logs and prepare for production

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,6 @@ const app = express();
 // middleware
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-
 // routes
 app.use("/api/birthdays", birthdayRoutes);
 app.use("/api/user", userRoutes);
@@ -30,9 +25,9 @@ mongoose
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening on port", process.env.PORT);
+      // console.log("connected to db & listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
-    console.log(error);
+    // console.log(error);
   });
